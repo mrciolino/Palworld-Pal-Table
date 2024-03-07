b7a32faddb0e0c3fb423265729f3f6de99feeb72
mrciolino <mrciolino@comcast.net>
2024-03-07 09:50:58 -0500

    theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { NameImageTemplate, ElementTemplate, PartnerSkillTemplate, ProfessionTemplate, DropTemplate } from './components.jsx';
 import { paldeckData, Elements, Professions } from './data.js';
 import { DataTable } from 'primereact/datatable';
-import Container from 'react-bootstrap/Container';
+import { ScrollTop } from 'primereact/scrolltop';
 import { Column } from 'primereact/column';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
 import { React, useState } from 'react';
-
+import 'primereact/resources/primereact.css'; 
 
 const App = () => {
 
   // dark mode switch
+  const [darkMode, setDarkMode] = useState(false);
 
+  const changeTheme = (theme) => {
+    let themeLink = document.getElementById('app-theme');
+    if (themeLink) {
+      themeLink.href = theme + '.css';
+    }
+  }
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    changeTheme(darkMode ? '/themes/bootstrap4-dark-blue/theme' : '/themes/bootstrap4-light-blue/theme');
+  }
 
   // Element filitering
   const [selectedElements, setSelectedElements] = useState([]);
@@ -29,9 +43,10 @@ const App = () => {
     return selectedElements.some(selected => pal.element.includes(selected));
   });
 
+
   // html code
   return (
-    <Container id="main" fluid>
+    <div fluid>
       <h1 className="text-center p-3">Palworld Pals</h1>
       <div className="text-center p-1 text-muted">
         Ctrl-Click on Multiple Columns to Sort <div className="vr"></div> Toggle the Element Buttons to Filter the Data <div className="vr"></div> Click on a Row for a Detailed Modal
@@ -45,11 +60,16 @@ const App = () => {
         ))}
       </div>
 
+      {/* dark mode switch */}
+      <div className="text-center p-3" >
+        <Button onClick={toggleDarkMode}>{darkMode ? 'Light' : 'Dark'} Mode</Button>
+      </div>
+
       <DataTable value={filteredPalsData} stripedRows sortMode="multiple" size="small" paginator rows={25} rowsPerPageOptions={[25, 111]} tableStyle={{ minWidth: '1rem', padding: '0 0' }}>
-        <Column field="paldeck" header="#" sortable bodyStyle={{ textAlign: 'center' }} style={{ width: '1%' }}></Column>
-        <Column header="Pal" body={NameImageTemplate} sortField='name' sortable style={{ width: '1%' }}></Column>
-        <Column header="Element" body={ElementTemplate} style={{ width: '1%' }}></Column>
-        <Column header="Skill" body={PartnerSkillTemplate} sortable sortField="type" style={{ width: '1%' }}></Column>
+        <Column field="paldeck" header="#" sortable bodyStyle={{ textAlign: 'center' }} style={{ width: '5%' }}></Column>
+        <Column header="Pal" body={NameImageTemplate} sortField='name' sortable style={{ width: '5%' }}></Column>
+        <Column header="Element" body={ElementTemplate} style={{ width: '5%' }}></Column>
+        <Column header="Skill" body={PartnerSkillTemplate} sortable sortField="type" style={{ width: '5%' }}></Column>
         {Professions.map((profession) => (
           <Column
             key={profession}
@@ -60,10 +80,12 @@ const App = () => {
             style={{ width: '1%' }}
           ></Column>
         ))}
-        {/* <Column header="Drops" body={DropTemplate}></Column> */}
+        <Column header="Drops" body={DropTemplate}></Column>
       </DataTable>
 
-    </Container >
+      <ScrollTop />
+
+    </div >
   )
 };
 
